7d19d3df5efac752c70b6f82d8a654cd5fa42a8d
Chi Su <sunstick@gmail.com>
2020-09-19 22:42:34 -0700

    add

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.header("accept-language"),
+    "software": req.header("user-agent")
+  });
+})
 
 
 // listen for requests :)
